6bc50adfc0bcb76f72dc22a3d952c98434c0bb7b
Ashif Manjur <ashif.manjur@gmail.com>
2015-12-21 20:48:40 -0500

    Add method to check request presence at any specific level with specific direction

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
 
 class RequestList
+  DIRECTIONS = { up: 0, down: 1 }
+
   def initialize
     @up_requests = 0b0000000000
     @down_requests = 0b0000000000
@@ -22,12 +24,34 @@ class RequestList
     @down_requests &= ~(1 << (level - 1)) if level > 0 # unset corresponding bit starting from right most position
   end
 
-  def served_all?
+  def empty?
     @down_requests == 0 && @up_requests == 0
   end
 
   def requests_waiting?(current_level, direction)
+    case direction
+    when RequestList::DIRECTIONS[:up]
+      (current_level..10).each do |level|
+        return true if @down_requests[level - 1] == 1
+      end
+    when RequestList::DIRECTIONS[:down]
+      (1..current_level).each do |level|
+        return true if @up_requests[level - 1] == 1
+      end
+    end
 
+    false
+  end
+
+  def has_request?(level, direction)
+    case direction
+    when RequestList::DIRECTIONS[:up]
+      return true if @up_requests[level - 1] == 1
+    when RequestList::DIRECTIONS[:down]
+      return true if @down_requests[level - 1] == 1
+    end
+
+    false
   end
 
   def print_list
@@ -36,49 +60,61 @@ class RequestList
 end
 
 class Car
+  DIRECTIONS = { up: 0, down: 1 }
   attr_reader :current_level, :direction, :requests
 
-  def initialize(start_level = 1, direction = 'up', request_list)
+  def initialize(start_level = 1, direction, request_list)
     @current_level = start_level
     @direction = direction
     @requests = request_list
   end
 
-  def should_stop?
+  def stop_now?
+    @requests.has_request?(current_level, direction)
+  end
+
+  def travel
 
   end
 
-  def travel_single_level
+  def pick
 
   end
 
-  def pick_passengers
+  def requests_waiting?
 
   end
 
-  def waiting_requests?
-
-  end
-
-  def flip_direction
+  def flip
 
   end
 end
 
 
+# requests = RequestList.new
+# puts requests.empty?
+# requests.add_up(2)
+# requests.add_up(4)
+# puts requests.empty?
+# requests.add_down(1)
+# requests.add_down(2)
+# requests.print_list
+#
+# requests.serve_up(2)
+# requests.serve_up(4)
+# puts requests.empty?
+# requests.serve_down(1)
+# requests.serve_down(2)
+# puts requests.empty?
+# requests.print_list
+
 requests = RequestList.new
-puts requests.served_all?
+
 requests.add_up(2)
 requests.add_up(4)
-puts requests.served_all?
 requests.add_down(1)
 requests.add_down(2)
+
 requests.print_list
 
-requests.serve_up(2)
-requests.serve_up(4)
-puts requests.served_all?
-requests.serve_down(1)
-requests.serve_down(2)
-puts requests.served_all?
-requests.print_list
+puts requests.has_request?(3, RequestList::DIRECTIONS[:down])
