dcb8c87627e252deae9186fd698bd4c4cfdfbfdb
Ashif Manjur <ashif.manjur@gmail.com>
2015-12-21 21:10:57 -0500

    Implement pick and flip method for empty request at any level and alternating the direction of the cars

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class RequestList
-  DIRECTIONS = { up: 0, down: 1 }
+  DIRECTIONS = { up: 'up', down: 'down' }
 
   def initialize
     @up_requests = 0b0000000000
@@ -60,13 +60,16 @@ class RequestList
 end
 
 class Car
-  DIRECTIONS = { up: 0, down: 1 }
+  DIRECTIONS = { up: 'up', down: 'down' }
+  MAX_FLOOR = 10
+
   attr_reader :current_level, :direction, :requests
 
   def initialize(start_level = 1, direction, request_list)
     @current_level = start_level
     @direction = direction
     @requests = request_list
+    pick if @requests.has_request?(@current_level, @direction)
   end
 
   def stop_now?
@@ -74,11 +77,28 @@ class Car
   end
 
   def travel
+    case direction
+    when Car::DIRECTIONS[:up]
+      if @current_level == Car::MAX_FLOOR
+        flip
+        return
+      end
 
+      pick if @requests.has_request?(@current_level, @direction)
+      @current_level += 1
+    when Car::DIRECTIONS[:down]
+      if @current_level == 1
+        flip
+        return
+      end
+
+      pick if @requests.has_request?(@current_level, @direction)
+      @current_level -= 1
+    end
   end
 
   def pick
-
+    @requests.send("serve_#{direction}".to_sym, @current_level)
   end
 
   def requests_waiting?
@@ -86,7 +106,12 @@ class Car
   end
 
   def flip
+    @direction = (@direction == Car::DIRECTIONS[:up] ? Car::DIRECTIONS[:down] : Car::DIRECTIONS[:up])
+    pick
+  end
 
+  def display
+    puts "#{current_level} #{direction == Car::DIRECTIONS[:up] ? '↑' : '↓'}"
   end
 end
 
@@ -110,11 +135,26 @@ end
 
 requests = RequestList.new
 
-requests.add_up(2)
+requests.add_up(1)
 requests.add_up(4)
-requests.add_down(1)
 requests.add_down(2)
 
 requests.print_list
 
-puts requests.has_request?(3, RequestList::DIRECTIONS[:down])
+# puts requests.has_request?(3, RequestList::DIRECTIONS[:down])
+
+car_one = Car.new(1, Car::DIRECTIONS[:up], requests)
+
+car_one.travel
+car_one.display
+
+car_one.travel
+car_one.display
+
+car_one.travel
+car_one.display
+
+car_one.travel
+car_one.display
+
+requests.print_list
